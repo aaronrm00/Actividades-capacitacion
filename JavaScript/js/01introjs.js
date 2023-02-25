21325305f4144e0aac72128c3b2bb8a37cfd40fa
aaronrm00 <125494788+aaronrm00@users.noreply.github.com>
2023-02-24 18:19:02 -0600

    Add files via upload

---
+++ JavaScript/js/01introjs.js
@@ -0,0 +1,16 @@
+//alerta en js
+/*alert("Hola mundo!");
+alert("Hola mundo 2!");*/
+
+
+//consola en js
+/* Mandar mensajes o comunicarnos con el navegador*/
+/*console.log('Hola saludamos a la consola');
+console.log(2+2);
+console.info('Mostrando una información');
+console.warn('Estamos mostrando una advertencia');
+console.error('Mostrando un error');
+console.clear();*/
+
+//documento write (estan todas las etiquetas)
+document.write('Hola este mensaje se mostrará en mi web');
